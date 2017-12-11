f8cfe2a90c48252b5bff85c9d75bd3e67655c85c
josh <josueh6758@gmail.com>
2017-12-11 11:07:11 -0500

    created template

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+
+template<class T>
+   struct Node {
+     Node(const T& v) : m_val(v), m_act(true), m_left(0), m_right(0) {}    
+     T        m_val; 
+     bool     m_act;
+     Node<T>* m_left;  // pointer to the left subtree. 
+     Node<T>* m_right; // pointer to the right subtree.
+   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//End of node
+template<class T>
+   class BST {
+   public:
+     BST();
+     BST(const BST&);
+     void operator=(const BST&);
+     ~BST();
+
+     bool remove(const T&);
+     void insert(const T&);
+     bool is_member(const T&) const;
+     long size() const { return m_active; }
+     void compress();  // removed all marked nodes.
+     Bag<T> sort() const; // produce a sorted Bag.
+   private:
+     Node<T>* m_root;  
+     long  m_active;   // count of active nodes.
+     long  m_inactive; // count of inactive nodes.  
+   };
+
+
+
+
+
